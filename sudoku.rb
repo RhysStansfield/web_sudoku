e99f9a5ed08f3554dbe075c2f5a760e78845a2d7
Rhys Stansfield <rhys@onlinesalesgrowth.com>
2013-10-31 11:40:14 +0000

    implemented partials

--- sudoku.rb
+++ sudoku.rb
@@ -5,10 +5,12 @@ require_relative './lib/sudoku'
 require_relative './lib/cell'
 
 use Rack::Flash
-
+register Sinatra::Partial
 set :partial_template_engine, :erb
 
 enable :sessions
+
+set :session_secret, "badoing"
 
 def random_sudoku
   seed = (1..9).to_a.shuffle + Array.new(81-9, 0)
@@ -43,9 +45,7 @@ end
 
 def prepare_to_check_solution
   @check_solution = session[:check_solution]
-  if @check_solution
-    flash[:notice] = "Incorrect values are highlighted in yellow"
-  end
+  flash[:notice] = "Incorrect values are highlighted in yellow" if @check_solution
   session[:check_solution] = nil
 end
 
@@ -71,7 +71,7 @@ end
 
 def box_order_to_row_order(cells)
   boxes = cells.each_slice(9).to_a
-  (0..8).to_a.inject([]) { |memo, i|
+  (0..8).to_a.inject([]) do |memo, i|
     first_box_index = i / 3 * 3
     three_boxes = boxes[first_box_index, 3]
     three_rows_of_three = three_boxes.map do |box|
@@ -80,7 +80,7 @@ def box_order_to_row_order(cells)
       box[first_cell_in_the_row_index, 3]
     end
     memo += three_rows_of_three.flatten
-  }
+  end
 end
 
 
